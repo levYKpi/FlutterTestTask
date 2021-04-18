e8cfa0eeb6600592a3a2ea6d7b8e96b6baca6902
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-18 19:41:59 +0300

    task: create text and background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Test Task",
+      home: Container(
+        child: Center(
+          child: Text(
+            "Hello App!",
+            style: TextStyle(
+              color: Colors.white,
+              shadows: <Shadow>[
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(4.0, 4.0),
+                  blurRadius: 2.0,
+                ),
+              ],
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+        ),
+        color: Color(0xffff00ff),
+      ),
+    );
+  }
 }
