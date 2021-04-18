b2b0d3268adbc2a89d5b13ddef0cc53463238b99
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-18 21:28:49 +0300

    task: disassemble MyApp and start MyRandomColor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+
+}
+
+class MyApp extends StatelessWidget{
+
+}
+
+class MyHomePage extends StatefulWidget{
+
+}
+
+class MyHomePageState extends State<MyHomePage>{
+
+}
+
+class MyRandomColor{
+    Color _color;
+
+    MyRandomColor(Color c){
+        this._color = Color(c.hashCode()*0x0ffffff + 0xff000000);
+    }
+
+    Color getColor(){
+        return this._color;
+    }
 }
