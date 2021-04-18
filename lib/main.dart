d88f39dfe97f52a2790d5aeca20a92d9cfacf672
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-18 21:32:33 +0300

    task: fixing MyRandomColor

--- lib/main.dart
+++ lib/main.dart
@@ -30,21 +30,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget{
-
-}
-
-class MyHomePageState extends State<MyHomePage>{
-
-}
+// class MyHomePage extends StatefulWidget{
+//
+// }
+//
+// class MyHomePageState extends State<MyHomePage>{
+//
+// }
 
 class MyRandomColor{
     Color _color;
 
     MyRandomColor(Color c){
-        this._color = Color(c.hashCode()*0x0ffffff + 0xff000000);
+        this._color = Color(c.hashCode * 0x0ffffff + 0xff000000);
     }
 
     Color getColor(){
         return this._color;
     }
+}
