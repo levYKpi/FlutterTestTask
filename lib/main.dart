f971ed91816564567b997d250d04d08567804230
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-19 20:45:24 +0300

    task: MyTextClass created, saving previous color added

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Test Task",
-      // home: Container(
-      //   child: Center(
-      //     child: Text(
-      //       "Hello App!",
-      //       style: TextStyle(
-      //         color: Colors.white,
-      //         shadows: <Shadow>[
-      //           Shadow(
-      //             color: Colors.black,
-      //             offset: Offset(4.0, 4.0),
-      //             blurRadius: 2.0,
-      //           ),
-      //         ],
-      //         fontStyle: FontStyle.normal,
-      //       ),
-      //     ),
-      //   ),
-      //   color: Color(0xffff00ff),
-      // ),
       home: MyHomePage(),
     );
   }
@@ -60,28 +41,82 @@ class _MyHomePageState extends State<MyHomePage> {
       onTap: () {
         this._getRandomColor();
       },
-      onDoubleTap: () {
+      onLongPress: () {
         this._setPreviousColor();
       },
       child: Container(
-        child: Center(
-          child: Text(
-            "Hey there",
-            style: TextStyle(
-              color: Colors.white,
-              shadows: <Shadow>[
-                Shadow(
-                  color: Colors.black,
-                  offset: Offset(5.0, 5.0),
-                  blurRadius: 10.0,
-                ),
-              ],
-              fontStyle: FontStyle.normal,
-              decoration: TextDecoration.none,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MyTextContainer(
+              "your color is: ${_myRandomColor.getColorVaue()}",
+              FontStyle.normal,
+              18.0,
+              // Colors.white,
+              // Colors.black,
+              // 10.0,
             ),
-          ),
+            MyTextContainer(
+              "Hey there",
+              FontStyle.normal,
+              32.0,
+              // Colors.white,
+              // Colors.black,
+              // 10.0,
+            ),
+            MyTextContainer(
+              "color in the list: ${_myRandomColor.getColorsListLength()}",
+              FontStyle.normal,
+              18.0,
+              // Colors.white,
+              // Colors.black,
+              // 10.0,
+            ),
+          ],
         ),
         color: _myRandomColor.getColor(),
+      ),
+    );
+  }
+}
+
+class MyTextContainer extends StatelessWidget {
+  String _message;
+  FontStyle _fontStyle;
+  double _fontSize;
+  Color _fontColor;
+  Color _shadowColor;
+  double _shadow;
+  MyTextContainer(String message, FontStyle fontStyle, double fontSize,
+      {Color fontColor: Colors.white,
+      Color shadowColor: Colors.black,
+      double shadow: 10.0}) {
+    _message = message;
+    _fontStyle = fontStyle;
+    _fontSize = fontSize;
+    _fontColor = fontColor;
+    _shadowColor = shadowColor;
+    _shadow = shadow;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _fontSize * 2,
+      child: Text(
+        _message,
+        style: TextStyle(
+          color: _fontColor,
+          shadows: <Shadow>[
+            Shadow(
+              color: _shadowColor,
+              offset: Offset(0, 0),
+              blurRadius: _shadow,
+            ),
+          ],
+          fontStyle: _fontStyle,
+          fontSize: _fontSize,
+          decoration: TextDecoration.none,
+        ),
       ),
     );
   }
@@ -114,7 +149,7 @@ class MyRandomColor {
     );
 
     this._colors.add(this._color);
-    if (this._colorListLength > 10) {
+    if (this._colorListLength >= 10) {
       this._colors.removeAt(0);
     } else {
       this._colorListLength++;
@@ -126,9 +161,19 @@ class MyRandomColor {
       this._colors.removeLast();
       this._colorListLength--;
     }
-    this._colors.forEach((element) {
-      print(this._colors.indexOf(element));
-    });
     this._color = this._colors.last;
+  }
+
+  int getColorsListLength() {
+    return this._colorListLength;
+  }
+
+  List<int> getColorVaue() {
+    List<int> list = [
+      _color.red,
+      _color.green,
+      _color.blue,
+    ];
+    return list;
   }
 }
