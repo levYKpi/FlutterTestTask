aa280dec4661cc141e20a0acac4330af1abada2d
Yasenko_Lev <l.study.kpi@gmail.com>
2021-04-19 22:29:10 +0300

    task: add simple widget test scenario

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -70,12 +70,33 @@ void myRandomColorTest() {
   });
 }
 
-// void myAppTest(){
-//     testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//
-//     });
-// }
+Widget getFirstContainer(WidgetTester tester){
+    return (
+        tester.widget(find.byType(GestureDetector)) as GestureDetector
+    ).child;
+}
+
+Color getMyAppColor (WidgetTester tester){
+    return (getFirstContainer(tester) as Container).color;
+}
+
+void myAppTest(){
+    testWidgets('Widget color test', (WidgetTester tester) async {
+        await tester.pumpWidget(MyApp());
+        //initial color white
+        assert(Colors.white == getMyAppColor(tester), "color isn't white");
+        await tester.tapAt(Offset(10.0, 10.0));
+        await tester.pump();
+        //after tap
+        assert(Colors.white != getMyAppColor(tester), "color is white");
+        //after long press
+        await tester.pump();
+        await tester.longPressAt(Offset(10.0, 10.0));
+        assert(Colors.white == getMyAppColor(tester), "color isn't white");
+    });
+}
 
 void main() {
   myRandomColorTest();
+  myAppTest();
 }
